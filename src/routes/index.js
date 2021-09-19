7cac200fbad12bc21a09d25e7a8b6e3af4c928d9
mfcortes <mfcortes@gmail.com>
2021-09-19 11:03:06 -0300

    .

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,12 +5,12 @@ const routes = Router();
 
 routes.get('/',(req,res)=>{
     console.log('Se solicito un request GET');
-    res.send('Hola Don Perro, como esta usted, lo estamos vigilando');
+    res.send('Hola Don Perro, como esta usted, lo estamos vigilando cuidado!!!');
 });
 
 
 routes.post('/',(req,res)=>{
-    console.log('Se solicito un requiest POST');
+    console.log('Se solicito un requiest POST don Dog');
     res.send(data);
 })
 
